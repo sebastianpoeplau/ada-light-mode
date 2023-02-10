5a98ed00cc0afae42ccdff2bf48c95427e342d3e
Sebastian Poeplau <sebastian.poeplau@mailbox.org>
2023-02-10 18:41:02 +0100

    Initial import

---
+++ ada-light-mode.el
@@ -0,0 +1,143 @@
+;;; ada-light-mode.el --- Light major mode for Ada  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Sebastian Poeplau
+
+;; Author: Sebastian Poeplau <sebastian.poeplau@mailbox.org>
+;; Keywords: languages
+;; URL: https://github.com/sebastianpoeplau/ada-light-mode
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a light-weight major mode for the Ada programming language. In
+;; contrast to ada-mode, it doesn't require a precompiled parser, and it doesn't
+;; do any intensive processing. As a consequence, it is faster but less
+;; accurate.
+
+;;; Code:
+
+(defvar ada-light-mode-keywords
+  ;; https://www.adaic.org/resources/add_content/standards/05rm/html/RM-2-9.html
+  '("abort" "else" "new" "return" "abs" "elsif" "not" "reverse" "abstract" "end"
+    "null" "accept" "entry" "select" "access" "exception" "of" "separate"
+    "aliased" "exit" "or" "subtype" "all" "others" "synchronized" "and" "for"
+    "out" "array" "function" "overriding" "tagged" "at" "task" "generic"
+    "package" "terminate" "begin" "goto" "pragma" "then" "body" "private" "type"
+    "if" "procedure" "case" "in" "protected" "until" "constant" "interface"
+    "use" "is" "raise" "declare" "range" "when" "delay" "limited" "record"
+    "while" "delta" "loop" "rem" "with" "digits" "renames" "do" "mod" "requeue"
+    "xor")
+  "Keywords of the Ada 2012 language.")
+
+(defvar ada-light-mode--font-lock-rules
+  (list (regexp-opt ada-light-mode-keywords 'symbols))
+  "Rules for search-based fontification in `ada-light-mode'.
+
+The format is appropriate for `font-lock-keywords'.")
+(defvar ada-light-mode-syntax-table     ; used automatically by define-derived-mode
+  (let ((table (make-syntax-table)))
+    ;; Comments start with "--".
+    (modify-syntax-entry ?- "< 12" table)
+    ;; Newlines end comments.
+    (modify-syntax-entry ?\n ">" table)
+    (modify-syntax-entry ?\r ">" table)
+    table)
+  "Syntax table used in `ada-light-mode'.")
+
+(defvar ada-light-mode--imenu-rules
+  `(("Functions"
+     ,(rx bol
+          (* space)
+          (? (? "not" (* space)) "overriding" (* space))
+          "function"
+          (+ space)
+          (group (+ (or word (syntax symbol)))))
+     1)
+    ("Procedures"
+     ,(rx bol
+          (* space)
+          (? (? "not" (* space)) "overriding" (* space))
+          "procedure"
+          (+ space)
+          (group (+ (or word (syntax symbol)))))
+     1)
+    ("Types"
+     ,(rx bol
+          (* space)
+          "type"
+          (+ space)
+          (group (+ (or word (syntax symbol)))))
+     1)
+    ("Packages"
+     ,(rx bol
+          (* space)
+          "package"
+          (+ space)
+          (group (+ (or word (syntax symbol))))
+          (+ space)
+          "is")
+     1))
+  "Imenu configuration for `ada-light-mode'.
+
+The format is appropriate for `imenu-generic-expression'.")
+(defun ada-light-mode--indent-line ()
+  "Indent a single line of Ada code."
+  ;; This is a really dumb implementation which just indents to the most recent
+  ;; non-empty line's indentation. It's better than the default though because
+  ;; it stops there, so that users who want completion on TAB can get it after
+  ;; indenting. (The default behavior is to insert TAB characters indefinitely.)
+  (let ((indent (save-excursion
+                  (beginning-of-line)
+                  (if (re-search-backward "^[^\n]" nil t) ; non-empty line
+                      (current-indentation)
+                    0))))
+    (if (<= (current-column) (current-indentation))
+        (indent-line-to indent)
+      (when (< (current-indentation) indent)
+        (save-excursion (indent-line-to indent))))))
+
+;;;###autoload
+(define-derived-mode ada-light-mode prog-mode "AdaL"
+  "Major mode for the Ada programming language.
+
+It doesn't define any keybindings. In comparison with `ada-mode',
+`ada-light-mode' is faster but less accurate."
+  ;; Set up commenting; Ada uses "--" followed by two spaces.
+  (setq-local comment-use-syntax t
+              comment-start "--"
+              comment-padding 2)
+
+  ;; Set up fontification.
+  (setq-local font-lock-defaults '(ada-light-mode--font-lock-rules nil t))
+
+  ;; And finally, configure imenu and indentation.
+  (setq-local imenu-generic-expression ada-light-mode--imenu-rules
+              standard-indent 3
+              indent-line-function 'ada-light-mode--indent-line))
+
+;; Register the mode for Ada code following GNAT naming conventions.
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.ad[bs]" . ada-light-mode))
+
+;; Configure eglot if available.
+(defvar eglot-server-programs)
+(when (require 'eglot nil t)
+  (add-to-list 'eglot-server-programs '(ada-light-mode "ada_language_server")))
+
+(provide 'ada-light-mode)
+;;; ada-light-mode.el ends here
